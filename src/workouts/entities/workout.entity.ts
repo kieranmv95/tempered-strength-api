a559f22b389551bdc7fcde8c709b23909b81674f
Andreas Neofytou <github@neofytou.com>
2024-03-01 07:43:04 +0200

    name is unique

--- src/workouts/entities/workout.entity.ts
+++ src/workouts/entities/workout.entity.ts
@@ -8,7 +8,7 @@ export class Workout {
   @Column({ type: 'varchar', length: 255, nullable: true })
   description?: string;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, unique: true })
   name: string;
 
   @Column({ type: 'varchar', length: 255, name: 'logging_type' })
